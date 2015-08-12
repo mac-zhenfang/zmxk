ee13ecfdd687e396fde45411fa821bb9bfafb38c
chenzhu <chenzhu@cisco.com>
2015-08-12 17:24:45 +0800

    Commit

--- service/init/deploy.sh
+++ service/init/deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+BASEDIR=$(dirname $0)
+PROPERTIES_DIR=$BASEDIR/properties
+SERVICE_DIR=$BASEDIR/../target/service-0.0.1-SNAPSHOT
 
 echo "###### stop tomcat ######"
 
-`$CATALINA_HOME/bin/catalina.sh stop`
+$CATALINA_HOME/bin/shutdown.sh
 
 echo "backup the last build"
 rm -rf /opt/builds/service-last-2
@@ -10,19 +13,16 @@ mkdir /opt/builds/service-last-2
 mv /opt/builds/service-last /opt/builds/service-last-2
 mv /opt/builds/service /opt/builds/service-last
 mkdir /opt/builds/service
-cp /opt/builds/current/service.war /opt/builds/service/
-echo "###### unzip the service.war #####"
-cd /opt/builds/service
-echo " current folder " `pwd`
-jar -xfv service.war
+cp -R $SERVICE_DIR/* /opt/builds/service/
+
 echo "##### replace the jdbc.properties #####"
 rm -rf /opt/builds/service/WEB-INF/classes/jdbc.properties
-cp -R /opt/builds/properties/* /opt/builds/service/WEB-INF/classes/
+cp -R $PROPERTIES_DIR/* /opt/builds/service/WEB-INF/classes/
 echo "##### delete the ROOT ######"
 rm -rf $CATALINA_HOME/webapps/ROOT/
 mkdir $CATALINA_HOME/webapps/ROOT
 echo "##### copy the current build into "
-cp -R * $CATALINA_HOME/webapps/ROOT/
+cp -R /opt/builds/service/* $CATALINA_HOME/webapps/ROOT/
 
 echo "##### start the tomcat ######"
 $CATALINA_HOME/bin/catalina.sh start
