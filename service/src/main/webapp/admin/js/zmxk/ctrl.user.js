a0bef9366dec1b933919f6b086569d6b067956ee
fangzhen <fangzhen@didichuxing.com>
2015-10-25 21:15:59 +0800

    [x] 用户ist可以根据时间排序

--- service/src/main/webapp/admin/js/zmxk/ctrl.user.js
+++ service/src/main/webapp/admin/js/zmxk/ctrl.user.js
@@ -25,6 +25,12 @@ zmxk
 								input : 'city'
 							});
 							$scope.criteria = {};
+							$scope.predicate = 'eventTime';
+							$scope.reverse = true;
+							$scope.order = function(predicate) {
+						        $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+						        $scope.predicate = predicate;
+						    };
 							$scope.init = function() {
 								$scope.userList = [];
 								var searchCriteria = {};
